6e48f4b8f64a64e19510073379bb9e746b78e8ca
mj23-dev <michael_jordan23@ukr.net>
2025-06-26 16:57:07 +0300

    Add files via upload

---
+++ hauptseite.py
@@ -0,0 +1,28 @@
+import streamlit as st, duckdb, pandas as pd, tempfile, os
+from utils.db import connect_temp_duckdb
+from streamlit_option_menu import option_menu
+
+st.set_page_config(page_title="KSO-Db v1.0", layout="wide")
+# menu = option_menu(None, ["Home", "Unternehmen", "Personen", 'Veranstaltungen', 'KSO-Management', 'Berichte'],
+#                         icons=['house', 'list-task', 'list-task', 'list-task','list-task'],
+#                         menu_icon="cast", default_index=0, orientation="horizontal",
+#                         styles={"container":{"max-width": "none"}})
+
+# === КРОК 1: Завантаження файлу бази ===
+st.title(":material/database: KSO DataWarehouse ⚡")
+
+st.header("To continue work with DB - please upload local database file 'kso_web.db':")
+
+expander = st.expander("🔌 DB connection:", expanded=True)
+uploaded_file = expander.file_uploader("Upload database file (kso_web.db)", type=["db"])
+if not uploaded_file:
+    # st.warning("⬅️ Upload local database file 'kso.db'.")
+    st.stop()
+else:
+    pass
+
+# === КРОК 2: Підключення до тимчасового duckdb ===
+conn, db_path = connect_temp_duckdb(uploaded_file)
+
+st.session_state["conn"] = conn
+st.session_state["db_path"] = db_path
