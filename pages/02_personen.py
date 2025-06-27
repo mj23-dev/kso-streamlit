db80402cedf07c23d7283048739deb0107c56b3c
mj23-dev <michael_jordan23@ukr.net>
2025-06-27 08:57:09 +0300

    Update 02_personen.py

--- pages/02_personen.py
+++ pages/02_personen.py
@@ -5,7 +5,7 @@ from utils.io import load_sql
 
 conn = st.session_state.get("conn")
 if conn is None:
-    st.warning("Спочатку потрібно завантажити базу через головну сторінку.")
+    st.warning("You need to upload database file from hauptsite!")
     st.stop()
 
 title = 'personen'
@@ -21,12 +21,18 @@ if name_filter:
     df = df[df["nachname"].str.contains(name_filter, case=False, na=False)]
 
 # відображаємо результат
-st.dataframe(df)
+event = st.dataframe(
+    df,
+    key="data",
+    on_select="rerun",
+    selection_mode=["single-row"],
+)
+
+if event['selection']['rows']:
+    selected_row_index = event['selection']['rows'][0]
+    st.write(f"Selected pers_id:= {df.iloc[selected_row_index,0]}")
 
 # Експорт
-buffer = io.BytesIO()
-
 df.to_excel(f'{title}.xlsx', index=False)
 with open(f'{title}.xlsx', "rb") as f:
     st.download_button("⬇️ Export results to XLSX", f, file_name=f'{title}.xlsx', mime="text/csv")
-
