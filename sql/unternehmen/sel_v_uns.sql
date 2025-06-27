b27ff3a6181bff68ce43c0d8911f0a9fe433978c
mj23-dev <michael_jordan23@ukr.net>
2025-06-27 08:53:08 +0300

    Update sel_v_uns.sql

--- sql/unternehmen/sel_v_uns.sql
+++ sql/unternehmen/sel_v_uns.sql
@@ -1,1 +1,6 @@
-select * from main.w_uns vv
+select wu.uns_id, wu.vollname_der_firma, wu.seite, wu.email, wu.telefonnummer, 
+		wu.juradr_land, wu.juradr_bundesland, wu.juradr_plz_ort, wu.juradr_strasse,
+		wu.rechtsform, wu.product_name_agg, wu.tatigkeitsbeschreibung, wu.registrierungsstatus,
+		wu.uns_mitg, wu.uns_mitg_maxd, wu.aktivitaten_id, wu.akt_titel, wu.akt_maxd,
+		wu.heaf, wu.hauptunternehmen_id
+from w_uns wu
