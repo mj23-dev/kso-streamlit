495496ed78d480a25c7de516f7e7568797da6df2
mj23-dev <michael_jordan23@ukr.net>
2025-06-27 09:09:20 +0300

    Update 03_veranstaltungen.py

--- pages/03_veranstaltungen.py
+++ pages/03_veranstaltungen.py
@@ -8,31 +8,23 @@ if conn is None:
     st.warning("You need to upload database file from hauptsite!")
     st.stop()
 
-title = 'personen'
-st.header("👨‍💼 Personen")
+title = 'veranstaltungen'
+st.title(":material/database: KSO Veranstaltungen")
+
+query = load_sql(f"{title}/sel_v_veranstaltung.sql")
 
 # виконуємо запит
-query = load_sql(f"{title}/sel_v_pers.sql")
 df = conn.execute(query).fetchdf()
 
 # Фільтри (приклад по одному полю)
 name_filter = st.text_input("🔍 Пошук за назвою:", "")
 if name_filter:
-    df = df[df["nachname"].str.contains(name_filter, case=False, na=False)]
+    df = df[df["vollname_der_firma"].str.contains(name_filter, case=False, na=False)]
 
-# відображаємо результат
-event = st.dataframe(
-    df,
-    key="data",
-    on_select="rerun",
-    selection_mode=["single-row"],
-)
-
-if event['selection']['rows']:
-    selected_row_index = event['selection']['rows'][0]
-    st.write(f"Selected pers_id:= {df.iloc[selected_row_index,0]}")
+st.dataframe(df)
 
 # Експорт
+buffer = io.BytesIO()
 df.to_excel(f'{title}.xlsx', index=False)
 with open(f'{title}.xlsx', "rb") as f:
-    st.download_button("⬇️ Export results to XLSX", f, file_name=f'{title}.xlsx', mime="text/csv"
+    st.download_button("⬇️ Export results to XLSX", f, file_name=f'{title}.xlsx', mime="text/csv")
